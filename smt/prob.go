da46a4d483a40aa07ace19ffa4446d5c710325d7
Frederick Robinson <frederick.robinson@gmail.com>
2019-02-17 21:16:17 +0000

    fix problems created by moving

--- smt/prob.go
+++ smt/prob.go
@@ -1,26 +1,30 @@
 package smt
 
+import (
+	"github.com/frrad/boolform/bf"
+)
+
 // Problem is a thin wrapper around Formula to allow for gradually building up a
 // complex formula out of many separate assertions.
 type Problem struct {
-	form Formula
+	form bf.Formula
 }
 
 // NewProb creates a new Problem instance to start building from.
 func NewProb() *Problem {
 	return &Problem{
-		form: True,
+		form: bf.True,
 	}
 }
 
 // Assert adds the requirement that the given assertion be true to the Problem
 // instance.
-func (p *Problem) Assert(ass Formula) {
-	p.form = And(p.form, ass)
+func (p *Problem) Assert(ass bf.Formula) {
+	p.form = bf.And(p.form, ass)
 }
 
 // AsFormula can be used to retrieve the Formula representation of the problem
 // we've built up.
-func (p *Problem) AsFormula() Formula {
+func (p *Problem) AsFormula() bf.Formula {
 	return p.form
 }
